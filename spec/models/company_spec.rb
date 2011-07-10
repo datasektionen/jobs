3565ce45ef00bda12e48e82e30efb14ba95b36d9
Victor Koronen <koronen@kth.se>
2011-07-10 23:16:42 +0200

    Add some simple tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "accepts valid names" do
+    company = create_company({})
+    company.should be_valid
+  end
+
+  it "rejects invalid names" do
+    company = create_company(:name => "")
+    company.should_not be_valid
+  end
+
+  it "enforces uniqueness of names" do
+    company_a1 = create_company(:name => "a")
+    company_a1.should be_valid
+
+    company_a2 = create_company(:name => "a")
+    company_a2.should_not be_valid
+
+    company_a1.destroy
+
+    company_a2 = create_company(:name => "a")
+    company_a2.should be_valid
+  end
+
+  it "accepts valid descriptions" do
+    company = create_company({})
+    company.should be_valid
+  end
+
+  it "rejects invalid descriptions" do
+    company = create_company(:description => "")
+    company.should_not be_valid
+  end
+
+  private
+
+  def create_company(options)
+    Company.create({:name => "Acme Inc.", :description => "Maker of all things"}.merge(options))
+  end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @company = Company.new(:name => "Acme Inc.", :description => "Maker of all things")
+  end
+
+  it "accepts valid names" do
+    job = create_job({})
+    job.should be_valid
+  end
+
+  it "rejects invalid names" do
+    job = create_job(:name => "")
+    job.should_not be_valid
+  end
+
+  it "accepts valid descriptions" do
+    job = create_job({})
+    job.should be_valid
+  end
+
+  it "rejects invalid descriptions" do
+    job = create_job(:description => "")
+    job.should_not be_valid
+  end
+
+  private
+
+  def create_job(options)
+    Job.create({:name => "Rails Programmer", :description => "Code on Jobs", :company => @company}.merge(options))
+  end
 end

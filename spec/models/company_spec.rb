1eb5b38f3da54ede4772fdfad6121c7deff552fb
Victor Koronen <koronen@kth.se>
2011-08-30 12:16:00 +0200

    Make model specs pass

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,42 +1,30 @@
 require 'spec_helper'
 
 describe Company do
-  it "accepts valid names" do
-    company = create_company({})
+  it "accepts valid attributes" do
+    company = Company.make
     company.should be_valid
   end
 
   it "rejects invalid names" do
-    company = create_company(:name => "")
+    company = Company.make :name => ""
+    company.should_not be_valid
+  end
+
+  it "rejects invalid descriptions" do
+    company = Company.make :description => ""
     company.should_not be_valid
   end
 
   it "enforces uniqueness of names" do
-    company_a1 = create_company(:name => "a")
+    pending("wip")
+    company_a1 = Company.make! :name => "a"
     company_a1.should be_valid
 
-    company_a2 = create_company(:name => "a")
+    company_a2 = Company.make :name => "a"
     company_a2.should_not be_valid
 
     company_a1.destroy
-
-    company_a2 = create_company(:name => "a")
     company_a2.should be_valid
-  end
-
-  it "accepts valid descriptions" do
-    company = create_company({})
-    company.should be_valid
-  end
-
-  it "rejects invalid descriptions" do
-    company = create_company(:description => "")
-    company.should_not be_valid
-  end
-
-  private
-
-  def create_company(options)
-    Company.create({:name => "Acme Inc.", :description => "Maker of all things"}.merge(options))
   end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,33 +1,18 @@
 require 'spec_helper'
 
 describe Job do
-  before do
-    @company = Company.new(:name => "Acme Inc.", :description => "Maker of all things")
-  end
-
-  it "accepts valid names" do
-    job = create_job({})
+  it "accepts valid attributes" do
+    job = Job.make
     job.should be_valid
   end
 
   it "rejects invalid names" do
-    job = create_job(:name => "")
+    job = Job.make :name => ""
     job.should_not be_valid
-  end
-
-  it "accepts valid descriptions" do
-    job = create_job({})
-    job.should be_valid
   end
 
   it "rejects invalid descriptions" do
-    job = create_job(:description => "")
+    job = Job.make :description => ""
     job.should_not be_valid
-  end
-
-  private
-
-  def create_job(options)
-    Job.create({:name => "Rails Programmer", :description => "Code on Jobs", :company => @company}.merge(options))
   end
 end
